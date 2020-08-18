efa1f8306728717de8712a7b1e91ef77a74cc075
angellovc <vc.angel@outlook.com>
2020-08-18 03:10:59 +0000

    New feat: Bash script 0-setup_web_static.sh added
    Bash script to setup the web server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#sets up the web servers for the deployment of web_static
+if ! command -v COMMAND &> /dev/null
+then
+	apt-get install nginx -y
+fi
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+echo "
+<html>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
+</html>
+" > /data/web_static/releases/test/index.html
+ln -s -f /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server;/ a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+/etc/init.d/nginx restart
