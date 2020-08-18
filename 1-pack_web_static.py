63b568ecbcf14cd040c1d268e655674dc2d4bdfc
angellovc <vc.angel@outlook.com>
2020-08-18 04:12:55 +0000

    Feat fix: 1-pack_web_static.py modified
    New condition was added to check if local command is working
    new returns added

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,6 +5,11 @@ from datetime import datetime
 
 
 def do_pack():
+    """ add all files in the folder web_static to the final archive """
     local('mkdir -p versions')
-    local('tar -czvf versions/web_static_\
+    result = local('tar -czvf versions/web_static_\
 {}.tgz web_static'.format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    if result.failed:
+        return None
+    else:
+        return result
