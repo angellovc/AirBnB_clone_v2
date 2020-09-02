2d5771031ce6c774884a3b157bac095fa7418ece
angellovc <vc.angel@outlook.com>
2020-09-02 19:20:07 +0000

    Feat fix: 5-number_template.py
    Number_template was modified to receive just integers

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ display n only if n is an integer """
     return render_template('5-number.html', n=n)
