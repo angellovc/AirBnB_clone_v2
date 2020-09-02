8be4e3dd64d882c5ab7b60e0171630cdd4dc4e74
angellovc <vc.angel@outlook.com>
2020-09-02 23:25:37 +0000

    New file:   7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from os import path
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+template_folder = path.abspath('./web_flask/templates/')
+app = Flask(__name__, template_folder=template_folder)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+
+@app.route('/states_list')
+def states_list():
+    return render_template('7-states_list.html', states=storage.all(State).values())
+
+
+@app.teardown_appcontext
+def teardown_appcontext(error):
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
