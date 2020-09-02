2c1ab88f0d33c824fa02172c36428eb11c6318d2
angellovc <vc.angel@outlook.com>
2020-09-02 19:48:30 +0000

    New file:   6-number_odd_or_even.py

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ display Hello HBNB! when / route is requested """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ display HBNB when /hbnb route is requested """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ C followed by the value of the text """
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ python followed by the value of the text """
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ display n only if n is an integer """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ display n only if n is an integer """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ display n only if n is an integer """
+    return render_template(
+        '6-number_odd_or_even.html',
+        n=n,
+        trim_blocks=True,
+        lstrip_blocks=True)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
