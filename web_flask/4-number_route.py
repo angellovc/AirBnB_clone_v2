15f4fc66b70dd4a175303876f3dfa811999ad4eb
angellovc <vc.angel@outlook.com>
2020-09-02 19:05:28 +0000

    Feat modified:   4-number_route.py
    number function's return was modified was modified
    unnecesary character was deleted

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -31,11 +31,10 @@ def python(text="is cool"):
     text = text.replace("_", " ")
     return 'Python {}'.format(text)
 
-
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """ display n only if n is an integer """
-    return '{} is a number”'.format(n)
+    return '{} is a number'.format(n)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
