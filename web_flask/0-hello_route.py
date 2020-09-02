7dd4ea76ecb9203a6956eec72c6b020598991bb4
angellovc <vc.angel@outlook.com>
2020-09-02 17:51:01 +0000

    Feat fix: 0-hello_route.py
    Flask run was moved to the last line to load all the
    routes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,13 @@
 """ starts a Flask web application """
 from flask import Flask
 
-
 app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ display “Hello HBNB!” """
+    return 'Hello HBNB!'
+
 if __name__ == '__main__':
-    app.run(host="0,0,0,0", port=5000)
-
-
-@app.route('/', strict_slashe=False)
-def display_hello():
-    """ display "Hello HBNB!" when '/' route
-    is requested
-    """
-    return "Hello HBNB!"
+    app.run(host="0.0.0.0", port=5000, debug=True)
